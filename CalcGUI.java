cf67e32548ddcee32b9b5e7d8bea80eda0f73bdb
Michael K <michael.kolling@kcl.ac.uk>
2020-03-03 09:24:01 +0000

    Initial sharing of project

---
+++ CalcGUI.java
@@ -0,0 +1,51 @@
+import java.net.URL;
+
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
+/**
+ * A Calculator GUI in JavaFX.
+ *
+ * @author mik
+ */
+public class CalcGUI extends Application
+{
+    private CalcEngine calc = new CalcEngine();
+
+    @FXML
+    private TextField display;
+    
+    @FXML
+    private void numberClick(ActionEvent event)
+    {
+        Button button = (Button) event.getSource();
+        display.setText(display.getText() + button.getText());
+    }
+    
+    @FXML
+    private void clear(ActionEvent event)
+    {
+        display.setText("0");
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        URL url = getClass().getResource("calc.fxml");
+        Pane root = FXMLLoader.load(url);
+        
+        Scene scene = new Scene (root);
+        
+        stage.setTitle("JavaFX PPA Calculator");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
